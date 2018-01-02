156c3a6d76112ae672b2c3e0d0d4184fc75f3f76
sehwan <student@M13063>
2018-01-02 11:00:46 +0900

    기본 세팅

--- src/multi/temp/controller/CtrlSh.java
+++ src/multi/temp/controller/CtrlSh.java
@@ -25,7 +25,6 @@ public class CtrlSh {
 	public ModelAndView login() throws Exception {
 		ModelAndView mnv = new ModelAndView("hhhh");
 		//	mnv.addObject("hi", "hi");
-		//ss dsfd
 		return mnv;
 	}
 }
