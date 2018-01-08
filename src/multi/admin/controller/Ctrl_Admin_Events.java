d2f600b9a248e9dd629cc4514891debc3ff1b0f1
snowind22 <snowind123@naver.com>
2018-01-08 11:17:11 +0900

    Updated on 01/08

--- src/multi/admin/controller/Ctrl_Admin_Events.java
+++ src/multi/admin/controller/Ctrl_Admin_Events.java
@@ -1,34 +1,8 @@
 package multi.admin.controller;
 
-import java.util.List;
-import static main.Single.*;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import main.Controller;
 import main.ModelAndView;
-import main.ModelAttribute;
 import main.RequestMapping;
-import main.RequestParam;
-import multi.admin.dao.Admin_FaqDAO;
-import multi.admin.dao.Admin_HostDAO;
-import multi.admin.dao.Admin_NoticeDAO;
-import multi.admin.dao.Admin_SpaceDAO;
-import multi.admin.dao.Admin_UserDAO;
-import multi.admin.dao.Admin_o2oQnADAO;
-/* 
-이벤트 관리
-
-이벤트 관리 페이지
- */
 
 @Controller
 public class Ctrl_Admin_Events {
--- src/multi/admin/controller/Ctrl_Admin_Users.java
+++ src/multi/admin/controller/Ctrl_Admin_Users.java
@@ -1,31 +1,16 @@
 package multi.admin.controller;
 
 import java.util.List;
-import static main.Single.*;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import main.Controller;
 import main.ModelAndView;
 import main.ModelAttribute;
 import main.RequestMapping;
-import main.RequestParam;
 import main.vo.UserVO;
-import multi.admin.dao.Admin_ClubDAO;
-import multi.admin.dao.Admin_FaqDAO;
-import multi.admin.dao.Admin_HostDAO;
-import multi.admin.dao.Admin_NoticeDAO;
-import multi.admin.dao.Admin_SpaceDAO;
 import multi.admin.dao.Admin_UserDAO;
-import multi.admin.dao.Admin_o2oQnADAO;
 
 
 /* 
